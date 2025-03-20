231b803f9369d1722a9d399d4d4f6c222bbd8775
inzam01 <shamasmarty@gmail.com>
2025-03-20 17:42:08 +0530

    Update productService.js

--- client/papakidukan/src/services/productService.js
+++ client/papakidukan/src/services/productService.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:5005/api/products";
-const API_URL = axios.create({ 
-    baseURL: "https://bite2go-website-back1.onrender.com/api"
-});
+
+//ye thk hai
+// const API_URL = axios.create({ 
+//     baseURL: "https://bite2go-website-back1.onrender.com/api"
+// });
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchProducts = async () => {
   try {
-    // const response = await axios.get(`${API_URL}`);
-      const response = await API_URL.get("/products");
+    const response = await axios.get(`${API_URL}/products`);
+      // const response = await API_URL.get("/products");
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
