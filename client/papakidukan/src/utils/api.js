7b86c98b4beb0a2f9ac8797ece15f54a6e52c896
inzam01 <shamasmarty@gmail.com>
2025-03-20 12:23:37 +0530

    Update api.js

--- client/papakidukan/src/utils/api.js
+++ client/papakidukan/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API = axios.create({baseURL: "http://localhost:5005/api"});
-const API = axios.create({baseURL: "https://bite2go-website-back1.onrender.com"}); 
+const API = axios.create({ baseURL: "https://bite2go-website-back1.onrender.com/api"}); 
 
 //register user
 export const registerUser = (userData) => API.post("auth/register", userData);
@@ -11,3 +11,16 @@ export const registerUser = (userData) => API.post("auth/register", userData);
 export const loginUser = (credential) => API.post("auth/login", credential);
 
 export default API;
+
+
+// import axios from 'axios';
+
+// const API = axios.create({ baseURL: "https://bite2go-website-back1.onrender.com/api" });
+
+// // Register user
+// export const registerUser = (userData) => API.post("/auth/register", userData);
+
+// // Login user with Mobile & Password
+// export const loginUser = (credential) => API.post("/auth/login", credential);
+
+// export default API;
