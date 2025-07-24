55d3373e95a0ab4a8ec089145e16022f3887ea1c
inzam01 <shamasmarty@gmail.com>
2025-07-24 13:50:39 +0530

    Update ProductCard.js

--- client/papakidukan/src/components/ProductCard.js
+++ client/papakidukan/src/components/ProductCard.js
@@ -4,6 +4,8 @@ import "../styles/ProductCard.css";
 import CartContext from '../context/CartContext';
 
 const ProductCard = ({ product }) => {
+     if (!product) return null;
+    
     const { addToCart, removeFromCart, cart } = useContext(CartContext);
     const quantity = cart?.items?.find(item => item.productId === product._id)?.quantity || 0;
 
